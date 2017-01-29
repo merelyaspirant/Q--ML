b0d4d8f92a4f4313cd3a4f3cee6e9ec41c1191c5
3Speedy <achalv18@gmail.com>
2017-01-29 23:41:46 +0530

    Now both players settles down to Draw, Player 2 first move is in centre for draw, (LOCAL MAXIMA), next is to add some randomness to explore, Q1.mat and Q2.mat files contains Q matrixes for players, got it after runnind twice loading same dictionary.x

--- tic-tac-toe/learn.py
+++ tic-tac-toe/learn.py
@@ -1,6 +1,7 @@
 from World import *
+import ast
 
-MAX_LPLAYS = 100000
+MAX_LPLAYS = 50000
 class Player:
 	def __init__(self, P):
 		self.id = P
@@ -23,11 +24,13 @@ class Player:
 	def reset(self):
 		self.World_score = 5.0
 		self.rec_states = 0
+		self.rec_state = []
 		self.last_s = 0
 		self.last_a = 0
 	
-	def save_state(self, s):
-		self.rec_state.append(s)
+	def save_state(self, s, a):
+		temp = [s,a]
+		self.rec_state.append(temp)
 		self.rec_states += 1
 		
 	def initialize_train(self):
@@ -48,13 +51,11 @@ class Player:
 				if val is None or self.Q[s,j] > val:
 					act = j
 					val = self.Q[s,j]
-#		if act == 0:
-#			print len(self.Q)
-#			print len(self.actions)
 		return act,val
 
 	def update_Q(self, s, a, r, future_val, alpha, discount):
 		self.Q[s,a] = self.Q[s,a] + alpha*(r + discount * future_val - self.Q[s,a])
+#		self.Q[s,a] = self.Q[s,a] + alpha*(r + discount * future_val)
 
 	def next_state(self, s1, a):
 		s2 = s1 + self.id*pow(3,a)
@@ -78,8 +79,8 @@ def get_state(b):
 slow_down = True
 def calc_move(g, P1, P2):
 	global slow_down
-	alpha = 0.9
-	discount = 0.7
+	alpha = 0.7
+	discount = 0.2
 
 	P = P1
 	reverse = 1
@@ -93,76 +94,115 @@ def calc_move(g, P1, P2):
 #		print "inside calc move \n",x,y
 		res = g.move(P.sym, x, y)
 
-		P.save_state(s1)
+		P.save_state(s1, act)
 
 		if res is 'tied':
-			r = -P.World_score
-			P.World_score += P.walk_reward
-			r += P.World_score
+			r = 0
 			future_val = 0.5
-			P.update_Q(s1, act, r, future_val, alpha, discount)
+			for m in range(2):
+				if m == 0:
+					P = P1
+				else:
+					P = P2
+				for k in range(P.rec_states):
+					[s_curr, act_curr] = P.rec_state[P.rec_states - 1 - k]
+					if k == 0:
+						P.update_Q(s_curr, act_curr, r, future_val, alpha, discount)
+					else:
+						[s_last, act_last] = P.rec_state[P.rec_states - k]
+						future_val = P.Q[s_last, act_last]
+						P.update_Q(s_curr, act_curr, r, future_val, alpha, discount)
+
+			if (plays % 700) == 0:
+				print "plays" , plays
+				if discount < 1:
+					discount += 0.01
+
+
 			if slow_down == True:
-				print "iter %d, %s Match Tied, Score %f\n" % (plays, P.sym, P.World_score)
+				print "iter %d,discount %f %s Match Tied, Score %f\n" % (plays, discount, P.sym, P.World_score)
 			P1.reset()
 			P2.reset()
-			if reverse:
-				reverse = 0
-				P = P2
-			else:
-				reverse = 1
-				P = P1
+			if (plays % 2) == 0:
+				if reverse:
+					reverse = 0
+					P = P2
+				else:
+					reverse = 1
+					P = P1
 #			sleep(1)
+			plays += 1
 
 		elif res is 'won':
-			r = -P.World_score
-			P.World_score += 0
-			r += P.World_score
+			r = 0.5
+			m = 0
 			future_val = 1.0
-			P.update_Q(s1, act, r, future_val, alpha, discount)
-			if P is P1:
-				r = -P2.World_score
-				P2.World_score += P2.walk_reward
-				r += P2.World_score
-				future_val = -1
-				P2.update_Q(P2.last_s, P2.last_a, r, future_val, alpha, discount)
-			else:
-				r = -P1.World_score
-				P1.World_score += P1.walk_reward
-				r += P1.World_score
-				future_val = -1
-				P1.update_Q(P1.last_s, P1.last_a, r, future_val, alpha, discount)
+
+			while m < 2:
+				for k in range(P.rec_states):
+					[s_curr, act_curr] = P.rec_state[P.rec_states - 1 - k]
+					if k == 0:
+						P.update_Q(s_curr, act_curr, r, future_val, alpha, discount)
+					else:
+						[s_last, act_last] = P.rec_state[P.rec_states - k]
+						future_val = P.Q[s_last, act_last]
+						P.update_Q(s_curr, act_curr, r, future_val, alpha, discount)
+
+				if P is P1:
+					P = P2
+				else:
+					P = P1
+				future_val = -1.0
+				r = 0
+				m += 1
+			if (plays % 700) == 0:
+				print "plays" , plays
+				if discount < 1:
+					discount += 0.01
+
 			if slow_down == True:
-				print "%iter %d, s Match Won, Score %f\n" % (plays, P.sym, P.World_score)
-				for i in P.actions:
-					print P.Q[0,i]
-			
+				print "iter %d, %s Match Won, Score %f discount %f\n" % (plays, P.sym, P.World_score, discount)
+				for i in P1.actions:
+					print P1.Q[0,i]
+
+				for i in P2.actions:
+					print P2.Q[0,i]
+		
 			P1.reset()
 			P2.reset()
-			if reverse:
-				reverse = 0
-				P = P2
-			else:
-				reverse = 1
-				P = P1
-			sleep(0.5)
+
+			if (plays % 2) == 0:
+				if reverse:
+					reverse = 0
+					P = P2
+				else:
+					reverse = 1
+					P = P1
+#			sleep(0.5)
+			plays += 1
+			
 
 		else:
-			r = -P.World_score
-			P.World_score += P.walk_reward
-			r += P.World_score
-			future_val ,a = P.max_Q(s2, g.board)
-			P.update_Q(s1, act, r, future_val, alpha, discount)
 			if P is P1:
 				P = P2
-
 			else:
 				P = P1
-#		print "Q1 %r\n" , Q1
-#		print "Q2 %r\n" , Q2
+
+
 		if slow_down == True:
 			sleep(0.25)
-		plays += 1
-		PR += 1
+	
+	fout = open('Q1-mat', 'w')
+	fout.write(str(P1.Q))
+	print "DATA1 written"
+	fout.close()
+
+	fout = open('Q2-mat', 'w')
+	fout.write(str(P2.Q))
+	print "DATA2 written"
+	fout.close()
+
+
 
 def calc2_move(g):
 	turn = 'X'
@@ -199,6 +239,15 @@ def train():
   P1.initialize_train()
   P2.initialize_train()
 
+  inp = str(raw_input("enter l to load models\n"))
+  if inp is 'l':
+    with open('Q1-mat','r') as inf:
+      P1.Q = ast.literal_eval(inf.read())
+    inf.close()
+    with open('Q2-mat','r') as inf:
+      P2.Q = ast.literal_eval(inf.read())
+    inf.close()
+    
   g = GUI('train')
   t1 = threading.Thread(target=calc_move, args=(g, P1, P2, ))
   t1.daemon = True
